e082c9cd0035d0d34628727820a876c377e3c73c
Carlton Schuyler <cschuyler@pivotal.io>
2020-01-27 16:56:08 -0600

    configuration local solution

--- src/main/java/io/pivotal/pal/tracker/EnvController.java
+++ src/main/java/io/pivotal/pal/tracker/EnvController.java
@@ -1,13 +1,34 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RestController;
+
 import java.util.Map;
 
+@RestController
 public class EnvController {
-    public EnvController(String port, String memoryLimit, String cfInstanceIndex, String cfInstanceAddr) {
+    private final String port;
+    private final String memoryLimit;
+    private final String cfInstanceIndex;
+    private final String cfInstanceAddr;
 
+    public EnvController(@Value("${port:NOT SET}") String port,
+                         @Value("${memory.limit:NOT SET}") String memoryLimit,
+                         @Value("${cf.instance.index:NOT SET}") String cfInstanceIndex,
+                         @Value("${cf.instance.addr:NOT SET}") String cfInstanceAddr) {
+
+        this.port = port;
+        this.memoryLimit = memoryLimit;
+        this.cfInstanceIndex = cfInstanceIndex;
+        this.cfInstanceAddr = cfInstanceAddr;
     }
 
     public Map<String, String> getEnv() {
-        return null;
+        return Map.of(
+                "PORT", port,
+                "MEMORY_LIMIT", memoryLimit,
+                "CF_INSTANCE_INDEX", cfInstanceIndex,
+                "CF_INSTANCE_ADDR", cfInstanceAddr
+        );
     }
 }
